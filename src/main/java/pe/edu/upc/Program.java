113586eb495e74cb6c2a6592c9989fa6da03985d
Sayda <Sayda@DESKTOP-UIN10N4>
2022-03-25 20:40:10 -0500

    creacion de una clase y objeto

---
+++ src/main/java/pe/edu/upc/Program.java
@@ -0,0 +1,16 @@
+package pe.edu.upc;
+
+public class Program {
+
+	public static void main(String[] args) {
+		
+		Student student=new Student();
+		
+		student.setFirstName("Sayda Bravo Lliuya");
+		
+		System.out.println(student.getFirstName());
+		
+
+	}
+
+}
